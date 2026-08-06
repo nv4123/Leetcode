caa8f1b8db53b1752f8a388c37fc3d386cf09dd4
Nidhi Verma <137144771+nv4123@users.noreply.github.com>
2026-08-06 21:38:55 +0530

    Time: 0 ms (100%), Space: 9 MB (3.33%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> getdigits(int num) {
+        vector<int>digits;
+
+        while(num>0){
+            digits.push_back(num % 10);
+            num /= 10;
+        }
+
+        return digits;
+    }
+    int smallestNumber(int n, int t) {
+        while (true) {
+            vector<int> digits = getdigits(n);
+
+            int product = 1;
+
+            for (int d : digits) {
+                product *= d;
+            }
+
+            if (product % t == 0) {
+                return n;
+            }
+
+            n++;
+        }
+    }
+};
